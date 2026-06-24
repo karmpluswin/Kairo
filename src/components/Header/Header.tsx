983ae7838afb0010e95938990e18994db5f4a266
Karmjeet Chauhan <karmpluswin@gmail.com>
2026-06-24 13:28:29 +0530

    feat: added link in logo text

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,25 +1,27 @@
-'use client';
+"use client";
 
-import { GitHubButton } from './GithubButton';
-import { SearchCommand } from './SearchCommand';
-import { FilterButton } from './FilterButton';
-import ThemeToggle from '@/components/ui/ThemeToggle';
+import { GitHubButton } from "./GithubButton";
+import { SearchCommand } from "./SearchCommand";
+import { FilterButton } from "./FilterButton";
+import ThemeToggle from "@/components/ui/ThemeToggle";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <header className="fixed inset-x-0 top-0 z-10 backdrop-blur-md bg-background/60 border-b border-border/40">
       <nav className="container mx-auto flex justify-between items-center h-16 px-4">
-
         <div className="flex items-center shrink-0">
-          <h1 className="flex text-2xl font-bold whitespace-nowrap">
-            <span
-              className="bg-gradient-to-r from-green-400 via-green-500 to-lime-500 bg-clip-text text-transparent mr-1 text-5xl"
-              style={{ fontFamily: "var(--font-sukajan)" }}
-            >
-              Kairo
-            </span>
-            {/* <span className="text-foreground">[カイロ]</span> */}
-          </h1>
+          <Link href="/" className="flex items-center">
+            <h1 className="flex text-2xl font-bold whitespace-nowrap">
+              <span
+                className="bg-gradient-to-r from-green-400 via-green-500 to-lime-500 bg-clip-text text-transparent mr-1 text-5xl"
+                style={{ fontFamily: "var(--font-sukajan)" }}
+              >
+                Kairo
+              </span>
+              {/* <span className="text-foreground">[カイロ]</span> */}
+            </h1>
+          </Link>
         </div>
 
         <div className="flex gap-2 items-center">
@@ -28,7 +30,6 @@ const Header = () => {
           <GitHubButton repoUrl="https://github.com/karmpluswin/kairo" />
           <ThemeToggle />
         </div>
-
       </nav>
     </header>
   );
